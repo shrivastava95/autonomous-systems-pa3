c98cf548d5d819d9c8eeafb16c8af6b095dead89
Ishaan Shrivastava <ishaanshri95@pop-os.localdomain>
2024-04-28 19:38:15 +0530

    add the connections.py to graph_data again

--- graph_data/graphing.py
+++ graph_data/graphing.py
@@ -5,3 +5,4 @@ from nodes2 import vertices
 print(vertices.split('\n').__len__() / 3)
 lines = vertices.split('\n')
 vertices = [[lines[3*i][1:-1], list(map(float,lines[3*i + 1].split()[:2]))] for i in range(len(lines) // 3)]
+
